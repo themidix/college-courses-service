ee75935583b4e50c603f31b13afca98e24c646f1
Dickson Diku <dixsonlukis@gmail.com>
2023-07-27 16:23:55 -0400

    added course jpa repo

--- src/main/java/com/example/collegecoursesservice/repository/CourseDAO.java
+++ src/main/java/com/example/collegecoursesservice/repository/CourseDAO.java
@@ -2,6 +2,8 @@ package com.example.collegecoursesservice.repository;
 
 import com.example.collegecoursesservice.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseDAO extends JpaRepository<Course, Integer> {
 }
