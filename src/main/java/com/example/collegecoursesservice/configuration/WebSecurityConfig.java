ecbe9ed959d7ece22859e96ebaf36738d81bae67
Dickson Diku <dixsonlukis@gmail.com>
2023-08-01 03:35:43 -0400

    java version updated to 11

--- src/main/java/com/example/collegecoursesservice/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/collegecoursesservice/configuration/WebSecurityConfig.java
@@ -1,10 +1,10 @@
-package com.example.collegecoursesservice.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity(debug = false) // Compliant
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-}
+//package com.example.collegecoursesservice.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity(debug = false) // Compliant
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//}
